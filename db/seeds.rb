9fb73e7e832e56e934b033a4b8a6d9737c896177
gildemberg.santos <gildemberg.santos@gmail.com>
2021-09-23 13:04:30 -0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setupcpf: '', email: '', dateOfBirth: '').
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }]cpf: '', email: '', dateOfBirth: '')
+#   Character.create(name: 'Luke', movie: movies.firstcpf: '', email: '', dateOfBirth: '')
+
+# Contact.create(name: '', cpf: '', email: '', dateOfBirth: '')
+Contact.create(name: 'Gildemberg', cpf: '058.577.663-62', email: 'gildemberg.santos@gmail.com', dateOfBirth: '1993/05/11')
