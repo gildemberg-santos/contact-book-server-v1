dca42862d473bf3472f1f4870be57d740166da7c
gildemberg-santos <gildemberg.santos@gmail.com>
2022-02-05 10:50:39 -0300

    teste

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,5 +1,40 @@
 class SlackController < ApplicationController
   def meet
-    render json: params
+    #   {
+    #     "blocks": [
+    #     {
+    #       "type": "section",
+    #       "text": {
+    #         "type": "mrkdwn",
+    #         "text": "*It's 80 degrees right now.*"
+    #       }
+    #     },
+    #     {
+    #       "type": "section",
+    #       "text": {
+    #         "type": "mrkdwn",
+    #         "text": "Partly cloudy today and tomorrow"
+    #       }
+    #     }
+    #   ]
+    # }
+    object = { blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: "*It's 80 degrees right now.*"
+        }
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: 'Partly cloudy today and tomorrow'
+        }
+      }
+    ] }
+    render json: object
+    # render json: params
   end
 end
